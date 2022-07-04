456d3d2ab3d932722dacce8d6415dd9d366ca5fc
Jackson Nascimento <jsnascimento1@hotmail.com>
2022-07-04 09:26:51 -0300

    criação do component footer

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,29 @@
+export const Container = styled.footer`
+  margin-top: 30px;
+  
+ 
+  a{
+    color: inherit
+  }
+  a + a {
+    padding: 15px;
+  }
+  
+  .icon-props{
+    width: 50px;
+    height: 50px;
+    width: 40px;
+    height: 40px;
+    color: var(--text);
+  }
+  h3{
+    color: var(--text);
+    font-size: 18px;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`; 
